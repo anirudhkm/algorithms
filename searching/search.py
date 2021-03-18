7ae91a9edf84a232ee2d704cfa69014f72265762
anirudhkm <anirudh796@gmail.com>
2021-03-17 22:23:38 -0700

    searching algos

--- searching/search.py
+++ searching/search.py
@@ -1,49 +1,55 @@
-import numpy as np
 
 
-class Searching(object):
+def linear_search(lst, key):
     """
-    Searching algorithms
+    Linear search algorithm.
     """
 
-    def __init__(self):
+    i = 0
+    while i < len(lst):
+        if key == lst[i]:
+            return i
+        i+=1
+    return -1
 
-        pass
 
-    @staticmethod
-    def linear_search(lst, key):
-        """
-        Linear search algorithm.
-        """
+def binary_search_iter(lst, key):
+    """
+    Binary search iter.
+    """
 
-        i = 0
-        while i < len(lst):
-            if key == lst[i]:
-                return i
-            i+=1
+    l, r = 0, len(lst)-1
+    while l <= r:
+        m = (l+r)//2
+        if key < lst[m]:
+            r = m-1
+        elif key > lst[m]:
+            l = m+1
+        else:
+            retun m
+    return -1
+
+def binary_search_recursive(lst, key, l, r):
+    """
+    Binary search in recursive style.
+    """
+
+    if l > r:
         return -1
+    m = (l+r)//2
+    if key > lst[m]:
+        return binary_search_recursive(lst, key, m+1, r)
+    elif key < lst[m]:
+        return binary_search_recursive(lst, key, l, m-1)
+    else:
+        return m
 
-    @staticmethod
-    def binary_search_iter(lst, key):
-        """
-        Binary search iter.
-        """
+def binary_search(self, lst, key, method="recursive"):
+    """
+    Binary search method.
+    """
 
-        l = 0
-        r = len(lst)-1
-        while l <= r:
-            
-    def binary_search(self, lst, key, method="recursive"):
-        """
-        Binary search method.
-        """
-
-        if method == "iterative":
-            binary_search_iter(lst, key)
-
-
-    
-
-a = Searching()
-b = a.linear_search([23, 42, 12, 45, 12, 56, 99], 67)
-print(b)
+    if method == "iterative":
+        binary_search_iter(lst, key)
+    else:
+        binary_search_recursive(lst, key)
