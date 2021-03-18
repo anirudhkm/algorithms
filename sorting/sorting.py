0fda7ec074833b5e4b57e49edc7ade77413c2b04
anirudhkm <anirudh796@gmail.com>
2021-03-17 23:03:17 -0700

    selection sort

---
+++ sorting/sorting.py
@@ -0,0 +1,18 @@
+
+
+def selection_sort(lst):
+    """
+    Selection sort implementation
+    """
+
+    l = len(lst)
+    for i in range(l-1):
+        pos = i
+        for j in range(i+1, l-1):
+            if lst[j] < lst[pos]:
+                pos = j
+        temp = lst[pos]
+        lst[pos] = lst[i]
+        lst[i] = temp
+    return lst
+
