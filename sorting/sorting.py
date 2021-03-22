758230658ce9c57bb8f9974f5b07946ddc7f51d6
anirudhkm <anirudh796@gmail.com>
2021-03-21 23:25:56 -0700

    Insertion sorts

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -15,3 +15,19 @@ def selection_sort(lst):
         lst[pos] = lst[i]
         lst[i] = temp
     return lst
+
+
+def insertion_sort(lst):
+    """
+    Perform insertion sort.
+    """
+    
+    for i in range(1, len(lst)):
+        pos = i
+        val = lst[i]
+        while pos > 0 and lst[pos-1] > val:
+            lst[pos] = lst[pos-1]
+            pos = pos - 1
+        lst[pos] = val
+    return lst
+
