ea02fc2b5d7f79fde900317a590b03383903f9c3
anirudhkm <anirudh796@gmail.com>
2021-03-22 22:10:03 -0700

    Bubble sort

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -31,3 +31,15 @@ def insertion_sort(lst):
         lst[pos] = val
     return lst
 
+def bubble_sort(lst):
+    """
+    Bubble sort implementation...
+    """
+    
+    for i in range(len(lst)-1, 0, -1):
+        for j in range(0, i):
+            if lst[j] > lst[j+1]:
+                temp = lst[j]
+                lst[j] = lst[j+1]
+                lst[j+1] = temp
+    return lst
